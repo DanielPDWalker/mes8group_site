1fc9c51b4e3541c2447692e95112833de50eefa0
danielpdwalker <danielpdwalker@gmail.com>
2020-09-09 17:18:14 +0100

    Formatting changes settings.py and base.html

--- mes8group/settings.py
+++ mes8group/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CRISPY_TEMPLATE_PACK="bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 USE_TZ = False
 
