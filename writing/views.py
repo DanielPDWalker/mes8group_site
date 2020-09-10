37c6a6d8a1ce3889423578f66482efbd017b2d4b
danielpdwalker <danielpdwalker@gmail.com>
2020-09-10 15:10:10 +0100

    writing views now passing published posts to template

--- writing/views.py
+++ writing/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from general_models.models import GeneralModel
 
 
 def writing_home(request):
+    writing_posts = GeneralModel.objects.order_by('-publish_date').filter(is_published=True, category='writing')
+
+    paginator = Paginator(writing_posts, 5)
+    page = request.GET.get('page')
+    paged_posts = paginator.get_page(page)
+
     context = {
-        'title': 'Writing'
+        'title': 'Writing',
+        'posts': paged_posts
     }
     return render(request, 'writing/writing.html', context)
 
