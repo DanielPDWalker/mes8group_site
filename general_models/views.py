d5b8a2801ade2e8bdc75999d07f621e63d79a8bc
danielpdwalker <danielpdwalker@gmail.com>
2020-09-17 10:52:06 +0100

    Added general_detail view to general_models app

--- general_models/views.py
+++ general_models/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from models import GeneralModel
+
+def general_detail(request, post_slug):
+    post = get_object_or_404(GeneralModel, slug=post_slug)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'general_post_detail.html', context)
